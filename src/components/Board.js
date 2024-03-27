db11d461821f6c57aab22b68a877417d642f5467
Ray <rlawlsdn9583@gmail.com>
2024-03-27 22:31:56 +0900

    merge working space

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+import Drawing from './Drawing';
+
+
 
 const Board = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+	const renderDrawing = (i) => {
+		let boardRows = [];
+		for(let n=0; n < i/10; n++){
+			let drawings = [] 
+			for(let m=0; m < 10; n++){
+				drawings.push(<Drawing value={n*10 + m}  />)
+			}
+			boardRows.push(<BoardRow>{drawings}</BoardRow>)
+		}
+		return boardRows
+	}
+	
+	return (
+		<BoardWrapper>
+			{renderDrawing(100)}
+        </BoardWrapper>
+	)
 }
 
 export default Board
+
+const BoardRow = styled.div`
+	::after{
+		clear: both;
+        content: "";
+        display: table;
+	}
+`;
+
+const BoardWrapper = styled.div`
+	border: 1px solid rgba(255,255,255,0.1);
+	box-shadow: rgba(0,0,0,0.19);
+	border-radius: 9px;
+	overflow: hidden;
+`;
+
